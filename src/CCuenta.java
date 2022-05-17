2e64dda88523b1a85ff6f1ac09f7588e8d40895c
alex <alex@localhost>
2022-05-17 12:16:09 +0100

    Cambiado nombre CCuenta3 a CCuenta

---
+++ src/CCuenta.java
@@ -0,0 +1,113 @@
+/**
+ * La clase CCuenta3. Esta clase hace bla y bla hola
+ * bla y bla
+ * bla y bla
+ * @author Admin
+ * @version 1.0, 31/08/2022
+ * @since 31/07/2020
+ */
+public class CCuenta {
+
+    private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoInterés;
+
+    public CCuenta() {
+        
+    }
+
+    public CCuenta(String nom, String cue, double sal, double tipo) {
+        nombre = nom;
+        cuenta = cue;
+        saldo = sal;
+    }
+
+    public double estado() {
+        return saldo;
+    }
+
+    /**
+     * Ingresar. Este método ingresa dinero
+     * @param cantidad Es el dinero a ingresar en la cuenta
+     * @throws Exception Lanza este error si la cantida recibida es negativa
+     */
+    public void ingresar(double cantidad) throws Exception {
+        if (cantidad < 0) {
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        }
+        saldo = saldo + cantidad;
+    }
+
+    /**
+     *
+     * @param cantidad Es el dinero a quitar de la cuenta
+     * @throws Exception Lanza este error si la cantidad a retirar es negativa o
+     * es superios al dinero que hay en la cuenta
+     */
+    public void retirar(double cantidad) throws Exception {
+        if (cantidad <= 0) {
+            throw new Exception("No se puede retirar una cantidad negativa");
+        }
+        if (estado() < cantidad) {
+            throw new Exception("No se hay suficiente saldo");
+        }
+        saldo = saldo - cantidad;
+    }
+
+    /** Getter del string cuenta
+     * @return the cuenta
+     */
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @return the saldo
+     */
+    public double getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * @return the tipoInterés
+     */
+    public double getTipoInterés() {
+        return tipoInterés;
+    }
+
+    /**
+     * @param cuenta the cuenta to set
+     */
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @param saldo the saldo to set
+     */
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    /**
+     * @param tipoInterés the tipoInterés to set
+     */
+    public void setTipoInterés(double tipoInterés) {
+        this.tipoInterés = tipoInterés;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
-//anadido comentario
-
-
+/**
+ * La clase Principal, por donde empieza la ejecución del proyecto
+ *
+ * @author Admin
+ * @version 1.0 since 31/07/2020
+ */
 public class Main {
 
     public static void main(String[] args) {
-        CCuenta3 miCuenta;
+        operativa_cuenta(1000);
+    }
+
+    /**
+     *
+     * @param cantidad No lo usamos para nada. Se creó este parámetro para ver
+     * como añadir un parámetro con el fin de repasar la refactorización
+     */
+    public static void operativa_cuenta(float cantidad) {
+        CCuenta miCuenta;
         double saldoActual;
-        
-        miCuenta = new CCuenta3("Antonio López","1000-2365-85-1230456789",2500,0);
+
+        miCuenta = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
         saldoActual = miCuenta.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
+        System.out.println("El saldo actual es" + saldoActual);
 
         try {
             miCuenta.retirar(2300);
